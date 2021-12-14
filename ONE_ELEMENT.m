8d2838076b8dc705659b8e6435edea46f27f4589
dwatanabe <watanabee0515@gmail.com>
2021-12-14 15:51:36 +0900

    one element

--- ONE_ELEMENT.m
+++ ONE_ELEMENT.m
@@ -2,7 +2,7 @@
 % One element example
 %
 % Nodal coordinates
-XYZ=[0 0 0;1 0 0;1 1 0;0 1 0;0 0 1;1 0 1;1 1 1;0 1 1];
+XYZ=[0 0 0;1 0 0;1 1 0;0 1 0;0 0 1;1 0 1;1 1 1;0 1 1]*0.01;
 %
 % Element connectivity
 LE=[1 2 3 4 5 6 7 8];
@@ -17,6 +17,7 @@ SDISPT=[1 1 0;1 2 0;1 3 0;2 2 0;2 3 0;3 3 0;4 1 0;4 3 0];
 %
 % Load increments [Start End Increment InitialFactor FinalFactor]
 TIMS=[0.0 0.5 0.1 0.0 0.5; 0.5 1.0 0.1 0.5 1.0]';
+%TIMS=[0.0 0.8 0.4 0.0 0.8; 0.8 1.1 0.1 0.8 1.1]';
 %TIMS=[0.0 1.0 1.0 0.0 1.0]';
 %
 % Contact elements
@@ -31,16 +32,18 @@ LEC=[];
 %     31(Linear hardening finite deformation)
 %     32(Saturated hardening finite deformation)
 %        PROP=[LAMDA MU BETA H Y0 CE1 FTOL]
-MID=1;
-PROP=[110.747E3   80.1938E3   0.0    10.E3  50.0E3  1.0  0.0001];
+%MID=1;
+%PROP=[110.747E3   80.1938E3   0.0    10.E3  50.0E3  1.0  0.0001];
 %     -N(Hyperelasticity with N parameters)
 %        PROP=[A10 A01 D]
+MID=31;
+PROP=[110.747E9 80.1938E9 0.0 1.E9 4.0E8];
 %MID=-2;
 %PROP=[80   20   10000];
 %
 % Set program parameters
-ITRA=30; ATOL=1.0E5; NTOL=6; TOL=1E-6;
-clc
+ITRA=70; ATOL=1.0E5; NTOL=6; TOL=1E-6;
+%clc
 %
 % Calling main function
 NOUT = fopen('output.txt','w');
